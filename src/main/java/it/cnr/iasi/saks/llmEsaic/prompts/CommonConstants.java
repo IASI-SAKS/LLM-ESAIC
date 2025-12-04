169419962e1086dc38ec9082a0c9f01a27ddbdb6
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-12-04 11:23:43 +0100

    Added a new class that (for the moment) shoud declare configuration parameters common to several classes

---
+++ src/main/java/it/cnr/iasi/saks/llmEsaic/prompts/CommonConstants.java
@@ -0,0 +1,17 @@
+package it.cnr.iasi.saks.llmEsaic.prompts;
+
+public class CommonConstants {
+	
+	protected static final String UNSET = "THIS ITEM HAS NOT BEEN SET";
+
+	protected static final String ESAIC_PATH = "src/main/resources/ESAIC";
+
+    public static String getUNSET() {
+		return UNSET;
+	}
+
+	public static String getESAICDefaultPath() {
+		return ESAIC_PATH;
+	}
+
+}
