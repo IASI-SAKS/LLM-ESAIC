bd90d45943896e4cdf093945d7ffa1a0ebffdde5
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-12-04 11:38:33 +0100

    Test improved, mainly in order to bind the factory introduced in the previous commit

--- src/test/java/it/cnr/iasi/saks/llmEsaic/tests/LoadingCaseTest.java
+++ src/test/java/it/cnr/iasi/saks/llmEsaic/tests/LoadingCaseTest.java
@@ -25,20 +25,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import it.cnr.iasi.saks.llmEsaic.impl.ESAICCaseAnalyzer;
+import it.cnr.iasi.saks.llmEsaic.utils.ESAICPrompterTestingFactory;
 
 public class LoadingCaseTest {
 	private static ESAICCaseAnalyzer prompter;
 	
 	@BeforeAll
     public static void setup() {
-    	prompter = new ESAICCaseAnalyzer();
+//    	prompter = new ESAICCaseAnalyzer();
+    	prompter = ESAICPrompterTestingFactory.getInstance().getESAICCaseAnalyzer();
     }
 
     @ParameterizedTest
     @CsvSource({"A1,true", "Z99,false"})
 	public void loadCasesTest(String caseID, boolean expected) {
-    	
-    	prompter.loadCase(caseID);
+    	System.err.println("Processing caseID: " + caseID + " ... ");
+    	prompter.loadCase(caseID);    	
+    	System.err.println("done caseID: " + caseID);
     	
     	assertEquals(expected, prompter.isLoadedCaseValid());    
     }
@@ -46,9 +49,13 @@ public class LoadingCaseTest {
     @ParameterizedTest
     @CsvSource({"A1", "A2", "A3", "A4"})
     public void processCasesWithoutAssessingSuggestionsTest(String caseID) {    	
+    	System.err.println("Processing caseID: " + caseID + " ... ");
     	prompter.loadCase(caseID);
     	prompter.processCase();
     	String response = prompter.fetchSuggestion();
+    	System.err.println("done caseID: " + caseID);
+
+    	System.err.println("Response of caseID: " + caseID + "\n" + response);
 
     	assertTrue(prompter.isSuggestionValid());    
     }
